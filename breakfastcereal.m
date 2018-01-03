2eea6f830fd51a78e8dd539607466a7c8a45b926
cbish87 <34955766+cbish87@users.noreply.github.com>
2018-01-03 12:24:19 -0800

    Add files via upload

---
+++ breakfastcereal.m
@@ -0,0 +1,15 @@
+% BreakfastCereal
+%
+%Provides examples
+frosted = 'FROSTED FLAKES';
+cheerios = 'CHEERIOS' ;
+
+frosted_scrambled = frosted;
+
+frosted_scrambled(9)='E';
+scrambled_cheerios=cheerios
+cheerios(1)= 'O';
+cheerios(5)= 'P';
+cheerios(1)= 'C';
+cheerios(5)= 'R';
+
